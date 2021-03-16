2abfe428dfaa526dc065f917361fe5d74557a09f
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-03-15 18:19:44 -0700

    created functions

--- strings_numbers_lab/monday-master/numbers/numbers.js
+++ strings_numbers_lab/monday-master/numbers/numbers.js
@@ -1,71 +1,128 @@
 // Implement the following functions
 
-const stringLib = () => 
+const numberLib = () => 
     {
+      for (let a = 0; a < 5; a++) {
+        for (let b = 0; b < 5; b++) {
+          // console.log(add(a, b));
+          // console.log(sub(a, b));
+          // console.log(mul(a, b));
+          // console.log(div(a, b));
+          // console.log(Exponentiation(a, b));
+          // console.log(Increment(a, b));
+        }
+      }
+      // console.log(NumberListA(1, 5));
+      // console.log(NumberListB(1, 10, 0));
+      // console.log(IsOdd(2));
+      // console.log(IsEven(4));
+      console.log(RandomNumberList(1, 20));
+      // console.log(NumberListC(2, 10, false));
 
 function add(a, b) {
     // add a to b
+    return a + b;
   }
 
 function sub(a, b) {
     // sub a from b
+    return a - b;
   }
 
 function mul(a, b) {
     // mul a by b
+    return a * b;
   }
 
 function div(a, b) {
     // dib a by b
+    return a / b;
   }
 
 function Exponentiation(a, b) {
     // expont a of b
+    return a ** b;
   }
 
 function Increment(a, b) {
     // inc a by b
+    return a += b;
   }
 
   
 function Decrement(a, b) {
     // dec a by b
+    return a -= b;
   }
   
 function NumberListA(start,end)
     {
     // return list of numbers starting at start and ending at end
+    let listA = [];
+    for (let i = start; i <= end; i++) {
+      listA.push(i);
+    }
+    return listA;
     }
 
-function NumberListB(start,count, skipEvery)
+function NumberListB(start, count, skipEvery)
     {
     // return list of numbers starting at start and ending at end but skipping every N (ie: skip every 2, or skip every 3)
+    let listB = [];
+    let skipCount = 0;
+    for (let i = start; i <= count; i++) {
+      if (skipCount === skipEvery) {
+        listB.push(i);
+      } else if (skipCount < skipEvery && i % skipEvery === 0) {
+        listB.filter((val) => !val % skipEvery);
+      }
+
+      return listB;
+    }
     }
 
 function NumberListC(start,count, shouldBeRandomOrNot)
     {
     // return list of numbers starting, continuing for (count) and a boolean to determine if the list of numbers should be random or not
+    let listc = [];
+    for (let i = start; i <= (start + count); i ++) {
+        if (shouldBeRandomOrNot === true) {
+          listc.push(Math.floor(Math.random() * (start - (start + count)) + (start + count)));
+        } else {
+          listc.push(i);
+        }
+        return listc;
+    }
     }
 
-function IsOdd(aNumber);
+function IsOdd(aNumber)
     {
     // is a number odd? true false
+    return (aNumber % 2 === 1) ? true : false;
     }        
 
-function IsEven(aNumber);
+function IsEven(aNumber)
     {
     // is a number even? true false
+    return (aNumber % 2 === 0) ? true : false;
     }        
 
-function RandomNumberList(start,end);
+function RandomNumberList(start,end)
     {
      // return list of random numbers between start and end
+     let randomNumFromTheList = [];
+     for (let i = start; i <= end; i++) {
+       randomNumFromTheList.push(Math.floor(Math.random() * i + start));
+     }
+     return randomNumFromTheList;
     }        
 
-function RandomNumberList(start,end,count, allowDuplicates)
+function RandomNumberListB(start,end,count, allowDuplicates)
     {
     // return count number of random numbers, between start and end. use allowDuplicates to choose if we should allow duplicates in the list or not
     }        
 
-
+      
     }
+
+    console.log(numberLib());
--- strings_numbers_lab/monday-master/strings/strings.js
+++ strings_numbers_lab/monday-master/strings/strings.js
@@ -3,41 +3,70 @@
 
 const stringLib = () => 
     {
+        // console.log(concatinate("Hello", "world"));
+        console.log(ASCIINumberToChar(66));
+        console.log(CharToAsciiNumber("A"));
+        // console.log(ReverseString("Hello"));
+        // console.log(GenerateOnlyUpperCase("this is upper case"));
+        // console.log(GenerateOnlyLowerCase("THIS IS LOWER CASE"));
+        // console.log(GenerateOnlyNumbers());
+        console.log(GenerateUpperAndLowerCase("no MUSIC NO life"));
 
-function concatanate(a,b)
+function concatinate(a,b)
 {
 // append one string to another
+    return a + b;
+    // return `${a} ${b}`;
 }
 
 
 function ASCIINumberToChar(Number)
 {
 // take one number, say, 127 and convert it to its ASCII char representation
+    return String.fromCharCode(Number);
+    
 }
 
 function CharToAsciiNumber(aChar)
 {
 // take one Character, say, 'A' and convert it to it's Number representation
+    return aChar.charCodeAt(0);
 }
 
-function ReverseString(origionalString)
+    function ReverseString(origionalString)
 {
 // return a string, ie: Hello becomes olleH
+
+
+// return origionalString.split("").reverse().join("");
+    let newString = "";
+    for (let i = origionalString.length - 1; i >= 0; i--) {
+        newString += origionalString[i];
+}
+    return newString;
 }
 
-function GenerateOnlyUpperCase()
+function GenerateOnlyUpperCase(upperCase)
 {
 //Ie: A,B,C,D,E,F,G...Z
-}
+    return upperCase.toUpperCase();
+    }
 
-function GenerateOnlyLowerCase()
+
+function GenerateOnlyLowerCase(lowerCase)
 {
 //Ie: a,b,c,d,e,f,g...z
+    return lowerCase.toLowerCase();
 }
 
-function GenerateOnlyNumbers()
+function GenerateOnlyNumbers(arr)
 {
 //Ie: 0,1,2,3...9
+    let arrList = [];
+    for (let i = 0; i < 10; i++) {
+        arrList.push(i)
+}
+    return arrList;
 }
 
 function GenerateOnlySymbols()
@@ -46,9 +75,19 @@ function GenerateOnlySymbols()
 }
 
 
-function GenerateUpperAndLowerCase()
+function GenerateUpperAndLowerCase(upperAndLowerCase)
 {
     //Ie: A,B,C,D,E,F,G...Z and  a,b,c,d,e,f,g...z
+    let upperCase = GenerateOnlyUpperCase(upperAndLowerCase)
+    let lowerCase = GenerateOnlyLowerCase(upperAndLowerCase)
+    return `${upperCase} ${lowerCase}`;
 }
 
 }
+
+console.log(stringLib());
+
+const string = "Miyabi";
+const charrr = string.charCodeAt(1);
+
+console.log(charrr);
