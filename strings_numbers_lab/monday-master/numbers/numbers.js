68709e3a36db9de15bb8aad026cdcd2ff9af8007
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-03-16 15:26:51 -0700

    added functions

--- strings_numbers_lab/monday-master/numbers/numbers.js
+++ strings_numbers_lab/monday-master/numbers/numbers.js
@@ -1,7 +1,6 @@
 // Implement the following functions
 
-const numberLib = () => 
-    {
+const numberLib = () => {
       for (let a = 0; a < 5; a++) {
         for (let b = 0; b < 5; b++) {
           // console.log(add(a, b));
@@ -13,122 +12,117 @@ const numberLib = () =>
         }
       }
       // console.log(NumberListA(1, 5));
-      console.log(NumberListB(3, 10, 2));
+      console.log(NumberListB(3, 10, 0));
       // console.log(randomNumberBetweenRange(1, 10))
       // console.log(IsOdd(2));
       // console.log(IsEven(4));
-      // console.log(RandomNumberList(1, 20));
-      // console.log(NumberListC(2, 10, true));
+      console.log(RandomNumberList(1, 20));
+      console.log(NumberListC(2, 10, false));
 
-function add(a, b) {
-    // add a to b
-    return a + b;
-  }
-
-function sub(a, b) {
-    // sub a from b
-    return a - b;
-  }
-
-function mul(a, b) {
-    // mul a by b
-    return a * b;
-  }
-
-function div(a, b) {
-    // dib a by b
-    return a / b;
-  }
-
-function Exponentiation(a, b) {
-    // expont a of b
-    return a ** b;
-  }
-
-function Increment(a, b) {
-    // inc a by b
-    return a += b;
-  }
-
-  
-function Decrement(a, b) {
-    // dec a by b
-    return a -= b;
-  }
-  
-function NumberListA(start,end)
-    {
-    // return list of numbers starting at start and ending at end
-    let listA = [];
-    for (let i = start; i <= end; i++) {
-      listA.push(i);
-    }
-    return listA;
+  function add(a, b) {
+      // add a to b
+      return a + b;
     }
 
-function NumberListB(start, end, skipEvery)
-    {
-    // return list of numbers starting at start and ending at end but skipping every N (ie: skip every 2, or skip every 3)
-    let listB = [];
-    // let skipCount = 0;
-    for (let i = start; i <= end; i++) {
-      if (skipEvery === 0) {
-        listB.push(i); 
-        
-      } else if (skipEvery > 0) {
-        listB.push(i);
-        // console.log(listB);
-        listB.filter((val, index) => index % skipEvery !== 0);
+  function sub(a, b) {
+      // sub a from b
+      return b - a;
+    }
+
+  function mul(a, b) {
+      // mul a by b
+      return a * b;
+    }
+
+  function div(a, b) {
+      // dib a by b
+      return a / b;
+    }
+
+  function Exponentiation(a, b) {
+      // expont a of b
+      return a ** b;
+    }
+
+  function Increment(a, b) {
+      // inc a by b
+      return a += b;
+    }
+
+    
+  function Decrement(a, b) {
+      // dec a by b
+      return a -= b;
+    }
+    
+  function NumberListA(start,end)
+      {
+      // return list of numbers starting at start and ending at end
+      let listA = [];
+      for (let i = start; i <= end; i++) {
+        listA.push(i);
       }
-    }
-    return listB;
-  }
+      return listA;
+      }
 
-  function randomNumberBetweenRange(min, max) {  
-    return Math.random() * (max - min) + min; 
-}  
-
-
-function NumberListC(start,count, shouldBeRandomOrNot)
-    {
-    // return list of numbers starting, continuing for (count) and a boolean to determine if the list of numbers should be random or not
-    let listc = [];
-    for (let i = start; i <= (start + count); i ++) {
-        if (shouldBeRandomOrNot === true) {
-          listc.push(Math.floor(randomNumberBetweenRange(start,(start+count))));
-        } else {
-          listc.push(i);
+  function NumberListB(start, count, skipEvery) {
+      // return list of numbers starting at start and ending at end but skipping every N (ie: skip every 2, or skip every 3)
+      let listB = [];
+      let skipCount = 0;
+      for (let i = start; (i <= (start + count)); i++) {
+        if (skipEvery === skipCount) {
+          listB.push(i); 
+          skipCount = 0;
         }
-        return listc;
+        skipCount ++;
+        if (skipCount > skipEvery) {
+          skipCount = 0;
+        }
+      }
+      return listB;
     }
-    }
 
-function IsOdd(aNumber)
-    {
-    // is a number odd? true false
-    return (aNumber % 2 === 1) ? true : false;
-    }        
+    function randomNumberBetweenRange(min, max) {  
+      return Math.floor(Math.random() * (max - min) + min); 
+  }  
 
-function IsEven(aNumber)
-    {
-    // is a number even? true false
-    return (aNumber % 2 === 0) ? true : false;
-    }        
 
-function RandomNumberList(start,end)
-    {
-     // return list of random numbers between start and end
-     let randomNumFromTheList = [];
-     for (let i = start; i <= end; i++) {
-       randomNumFromTheList.push(Math.floor(Math.random() * i + start));
-     }
-     return randomNumFromTheList;
-    }        
+  function NumberListC(start,count, shouldBeRandomOrNot) {
+      // return list of numbers starting, continuing for (count) and a boolean to determine if the list of numbers should be random or not
+      let listC = [];
+      for (let i = start; i <= (start + count); i++) {
+          if (shouldBeRandomOrNot === true) {
+            listC.push(randomNumberBetweenRange(start,(start+count)));
+          } else {
+            listC.push(i);
+          }
+          
+      }
+      return listC;
+      }
 
-function RandomNumberListB(start,end,count, allowDuplicates)
-    {
-    // return count number of random numbers, between start and end. use allowDuplicates to choose if we should allow duplicates in the list or not
-    }        
+  function IsOdd(aNumber) {
+      // is a number odd? true false
+      return (aNumber % 2 === 1) ? true : false;
+      }        
+
+  function IsEven(aNumber) {
+      // is a number even? true false
+      return (aNumber % 2 === 0) ? true : false;
+      }        
+
+  function RandomNumberList(start,end) {
+      // return list of random numbers between start and end
+      let randomNumFromTheList = [];
+      for (let i = start; i <= end; i++) {
+        randomNumFromTheList.push(Math.floor(Math.random() * i + start));
+      }
+      return randomNumFromTheList;
+      }        
+
+  function RandomNumberListB(start,end,count, allowDuplicates) {
+      // return count number of random numbers, between start and end. use allowDuplicates to choose if we should allow duplicates in the list or not
+      }        
 
       
     }
--- strings_numbers_lab/monday-master/strings/strings.js
+++ strings_numbers_lab/monday-master/strings/strings.js
@@ -1,88 +1,112 @@
 // Implement the following functions
 // your code. Refactor this file as you see fit. Export or modify any function as you see fit. Add additonal functions as you see fit. 
 
-const stringLib = () => 
-    {
+const stringLib = () => {
+        // console.log(GenerateCharactersTable(65, 70));
         // console.log(concatinate("Hello", "world"));
-        console.log(ASCIINumberToChar(66));
-        console.log(CharToAsciiNumber("A"));
+        // console.log(ASCIINumberToChar(0));
+        // console.log(CharToAsciiNumber("Miyabi the human"));
         // console.log(ReverseString("Hello"));
         // console.log(GenerateOnlyUpperCase("this is upper case"));
         // console.log(GenerateOnlyLowerCase("THIS IS LOWER CASE"));
-        // console.log(GenerateOnlyNumbers());
-        console.log(GenerateUpperAndLowerCase("no MUSIC NO life"));
+        console.log(GenerateOnlyNumbers());
+        console.log(GenerateOnlySymbols());
+        // console.log(GenerateUpperAndLowerCase());
 
-function concatinate(a,b)
-{
-// append one string to another
-    return a + b;
-    // return `${a} ${b}`;
-}
-
-
-function ASCIINumberToChar(Number)
-{
-// take one number, say, 127 and convert it to its ASCII char representation
-    return String.fromCharCode(Number);
-    
-}
-
-function CharToAsciiNumber(aChar)
-{
-// take one Character, say, 'A' and convert it to it's Number representation
-    return aChar.charCodeAt(0);
-}
-
-    function ReverseString(origionalString)
-{
-// return a string, ie: Hello becomes olleH
-
-
-// return origionalString.split("").reverse().join("");
-    let newString = "";
-    for (let i = origionalString.length - 1; i >= 0; i--) {
-        newString += origionalString[i];
-}
-    return newString;
-}
-
-function GenerateOnlyUpperCase(upperCase)
-{
-//Ie: A,B,C,D,E,F,G...Z
-    return upperCase.toUpperCase();
+    function GenerateCharactersTable(start, end) {
+        let table = [];
+        for (let i = start; i <= end; i++) {
+            table.push(String.fromCharCode(i));
+        }
+        return table;
     }
 
 
-function GenerateOnlyLowerCase(lowerCase)
-{
-//Ie: a,b,c,d,e,f,g...z
-    return lowerCase.toLowerCase();
-}
 
-function GenerateOnlyNumbers(arr)
-{
-//Ie: 0,1,2,3...9
-    let arrList = [];
-    for (let i = 0; i < 10; i++) {
-        arrList.push(i)
-}
-    return arrList;
-}
+    function concatinate(a,b) {  // append one string to another   
+        return a.concat(b);
+    }
 
-function GenerateOnlySymbols()
-{
-//Ie: $, %, !, #, etc
-}
+    function ASCIINumberToChar(Number) {  // take one number, say, 127 and convert it to its ASCII char representation
+        // return String.fromCharCode(Number);
+        let arr = [];
+        for (i = Number; i < 128; i++) {
+            arr.push(String.fromCharCode(i));
+        }
+        return arr;
+    }
+
+    function CharToAsciiNumber(aChar) {  // take one Character, say, 'A' and convert it to it's Number representation
+        // return aChar.charCodeAt(0);
+        let arr2 = [];
+        for (i = 0; i < aChar.length-1; i++) {
+            arr2.push(aChar.charCodeAt(i));
+        }
+        return arr2;
+    }
+
+    function ReverseString(origionalString) {
+    // return a string, ie: Hello becomes olleH
+
+    // return origionalString.split("").reverse().join("");
+        let newString = "";
+        for (let i = origionalString.length - 1; i >= 0; i--) {
+            newString += origionalString[i];
+        }
+        return newString;
+    }
+
+    function GenerateOnlyUpperCase(upperCase) {
+    //Ie: A,B,C,D,E,F,G...Z
+        return upperCase.toUpperCase();
+    }
 
 
-function GenerateUpperAndLowerCase(upperAndLowerCase)
-{
-    //Ie: A,B,C,D,E,F,G...Z and  a,b,c,d,e,f,g...z
-    let upperCase = GenerateOnlyUpperCase(upperAndLowerCase)
-    let lowerCase = GenerateOnlyLowerCase(upperAndLowerCase)
-    return `${upperCase} ${lowerCase}`;
-}
+    function GenerateOnlyLowerCase(lowerCase) {
+    //Ie: a,b,c,d,e,f,g...z
+        return lowerCase.toLowerCase();
+    }
 
+    function GenerateOnlyNumbers() {
+    //Ie: 0,1,2,3...9
+        // let arrList = [];                 // way 1
+        // for (let i = 0; i < 10; i++) {
+        //     arrList.push(i)
+        // }
+        // return arrList;
+        // return GenerateCharactersTable(48, 57)  
+
+        let array = [];                        // //way2
+        for (i = 0; i < 128; i++) {
+            const reg = new RegExp(/[0123456789]/g);
+            reg.test(String.fromCharCode(i)) && array.push(String.fromCharCode(i));
+        }
+        return array;
+
+    }
+
+    function GenerateOnlySymbols() {
+        //Ie: $, %, !, #, etc
+        // let symbolGroup1 = GenerateCharactersTable(33, 47);     // way 1
+        // let symbolGroup2 = GenerateCharactersTable(58, 64);
+        // let symbolGroup3 = GenerateCharactersTable(91, 96);
+        // let symbolGroup4 = GenerateCharactersTable(123, 126);
+        // return `${symbolGroup1}${symbolGroup2}${symbolGroup3}${symbolGroup4}`
+
+        let arr3 = [];                                             // way 2
+        for (i = 0; i < 128; i++) {
+            const reg = new RegExp(/[!"#$%&'()\*\+\-\.,\/:;<=>?@\[\\\]^_`{|}~]/g);
+            reg.test(String.fromCharCode(i)) && arr3.push(String.fromCharCode(i));
+        }
+        return arr3;
+    }
+
+    function GenerateUpperAndLowerCase() {
+        //Ie: A,B,C,D,E,F,G...Z and  a,b,c,d,e,f,g...z
+        let upperCase = GenerateCharactersTable(65, 90)
+        let lowerCase = GenerateCharactersTable(97, 122)
+        return `${upperCase} ${lowerCase}`;
+    }
 }
 
 console.log(stringLib());
