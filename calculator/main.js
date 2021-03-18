7f470ec66bd35dc342f8b94d10e1f81765ff12c4
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-03-18 15:57:10 -0700

    added comments on js file

--- calculator/main.js
+++ calculator/main.js
@@ -6,45 +6,42 @@ let lastOperator = "";
 let lastDot = "";
 
 const pressedANumber = (num) => {
-    console.log(num);
-    lastOperator = "";
-    if (display.textContent === "0") {
+    lastOperator = ""; // reset the last operator
+    if (display.textContent === "0") { // When you press a number for the first time
         calculation = "";
-        display.textContent = calculation;
-    } else {
-    }
-    display.textContent = display.textContent + num;
+        display.textContent = calculation; // The default 0 will disapper
+    } 
+    display.textContent = display.textContent + num; // A number will be added
 }
 
 const pressedAOperator = (operator) => {
     
-    if (display.textContent === "0") {
-        if(operator === "*" || operator === "/") {
-            calculation = "0";
+    if (display.textContent === "0") {  // When you press an operator for the first time
+        if(operator === "*" || operator === "/") { // But if it's an either * or /
+            calculation = "0";  // Keeps 0
         } else {
-            calculation = operator;
+            calculation = operator;  // If its either + or -, 
         }
-        display.textContent = calculation;
+        display.textContent = calculation;  // It just shows one of the operators passed
     }
-
     console.log(lastOperator + ":" + operator);
-   if (lastOperator === operator) {
-       lastOperator = "";
-       display.textContent[display.textContent.length - 1] = operator;
+   if (lastOperator === operator) {  // When an operator was pressed more than twice in a row
+       lastOperator = ""; // last operator = "", to try not to the same operators in a row
    } else if (isNaN(display.textContent[display.textContent.length - 1]) == true) { // Means the last character is an operator except the previous one
-        let position = display.textContent.lastIndexOf(lastOperator);
-        console.log(lastOperator);
-        console.log(position);
-            // display.textContent[position] = operator;
-            display.textContent = display.textContent.replace(lastOperator, operator);
+            if (lastOperator === "") {  // When you press + or - for the first time, "operator" should be assigned to avoid two operators shown in a row
+                calculation = operator;
+                lastOperator = operator;
+            } else {
+            display.textContent = display.textContent.replace(lastOperator, operator); // Replace an oprerator so you can change it
             lastOperator = operator;
             console.log(true);
+            }
     } else {
-        lastOperator = operator;
-        display.textContent = display.textContent + lastOperator;
+        lastOperator = operator;  // When an operator is pressed for the first time after some number(s),
+        display.textContent = display.textContent + lastOperator; // The numbers and the operator(s) will be shown
     } 
     
-    if (operator === "=") {
+    if (operator === "=") {  // pressedEqual is executed when = is pressed
         pressedEqual(operator);
     }
     lastDot = "";
@@ -52,24 +49,24 @@ const pressedAOperator = (operator) => {
 
 const pressedDot = (dot) => {
 
-    if (lastDot === ".") {
-        display.textContent[display.textContent.length - 1] = lastDot;
-        lastDot = "";
+    if (lastDot === ".") { // If there is already a dot there
+        display.textContent[display.textContent.length - 1] = lastDot; // replase the dot
+        lastDot = ""; // Erase the dot
         
     } else {
-        display.textContent = display.textContent + dot;
+        display.textContent = display.textContent + dot; // Adds a dot to the string
     }
-    lastDot = ".";
+    lastDot = "."; // Assign the dot for the next time
 }
 
-const pressedC = () => {
+const pressedC = () => {     // Reset everything
     display.textContent = "0";
     calculation = "";
     lastOperator = "";
     lastDot = "";
 }
 
-const pressedEqual = () => {
+const pressedEqual = () => {   // function to calculate
     display.textContent = eval(display.textContent);
 }
 
