f14c68a92e40f80f370700a234df4ab23175c35c
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-03-17 23:29:03 -0700

    work on the second part of prime number func

--- prime_number/prime_number.js
+++ prime_number/prime_number.js
@@ -1,10 +1,10 @@
 const isPrimeNumber = (num) => {
 if (num < 2 || !Number.isInteger(num)) {
-    return `${num} is not a prime numberrrr`;
+    return `${num} is not a prime number`;
 } else if (num === 2) {
     return "2 is a prime number";
 } else if (num % 2 === 0) {
-    return `${num} is not a prime numberrrr`;  
+    return `${num} is not a prime number`;  
 } else if (num % 2 === 1) {
     let rangeToCheckPrime = num / 2;
     for (i = 2; i <= rangeToCheckPrime - 1; i++) {
@@ -19,42 +19,30 @@ console.log(isPrimeNumber(123459997));
 
 
 const showPrimeNumberFromTheRange = (start, end) => {
-    let primeNumberList = [];
+    let list = [];
+    let primeNumList;
     for (i = start; i <= end; i++) {
-        primeNumberList.push(i);
-        // if (i < 2 || !Number.isInteger(num)) {
-        //     return `${num} is not a prime numberrrr`;
-        // } else if (num === 2) {
-        //     return "2 is a prime number";
-        // } else if (num % 2 === 0) {
-        //     return `${num} is not a prime numberrrr`;  
-        // } else if (num % 2 === 1) {
-        // if (num % i === 0) {
-        //     return `${num} is not a prime number`;
-        // } 
-        // }
-        // return `${num} is a prime number`;
+        list.push(i);
     }
-    console.log(primeNumberList);
-        primeNumberList.filter((num) => {
+    console.log(list);
+        list.filter((num) => {
             if (num < 2 || !Number.isInteger(num)) {
                 return false;
             } else if (num === 2) {
                 return true;
             } else if (num % 2 === 0) {
-                return false;  
+                return false;
             } else if (num % 2 === 1) {
-                let rangeToCheckPrime = num / 2;
-                for (i = 2; i <= rangeToCheckPrime - 1; i++) {
+                for (i = 2; i <= end; i++) {
                     if (num % i === 0) {
                         return false;
                     } 
                 }
+            } else {
                 return true;
             }
-        })
-    return primeNumberList;
-
+        });
+        return list;
 }
 
-console.log(showPrimeNumberFromTheRange(4, 10));
+console.log(showPrimeNumberFromTheRange(3, 10));
